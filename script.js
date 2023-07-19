83e35e1bde5a43065b4f2624a88154ab93c281a8
Amy Snarr <amy.snarr16@gmail.com>
2023-07-19 16:48:40 -0600

    added copy buttons

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ for (let i = 0; i < checkboxes.length; i++) {
     }
   });
 }
+
+const copyButtons = document.getElementsByClassName('copy-button');
+
+for (let i = 0; i < copyButtons.length; i++) {
+  copyButtons[i].addEventListener('click', function() {
+    const targetId = this.getAttribute('data-copy');
+    const target = document.getElementById(targetId);
+
+    // Create a temporary textarea to hold the text to be copied
+    const tempTextarea = document.createElement('textarea');
+    tempTextarea.value = target.textContent;
+    document.body.appendChild(tempTextarea);
+
+    // Select the text in the textarea
+    tempTextarea.select();
+    tempTextarea.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
+
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextarea);
+  });
+}
